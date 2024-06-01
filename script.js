4e6dfe79ed51fb8190db7e475b6511811fbd9ff5
Charlie <charliepeng222@gmail.com>
2024-05-31 20:23:03 -0400

    Add functions for operations

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let number1;
+let operator;
+let number2;
+
+function operate(operator, number1, number2) {
+    switch (operator) {
+        case "+":
+            return add(number1, number2); 
+        case "-":
+            return subtract(number1, number2);
+        case "*":
+            return multiply(number1, number2);
+        case "/":
+            return divide(number1, number2);
+    }   
+}
